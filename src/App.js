d5ca2b914865b2d380514cb9e1aad0aac0d82ef9
Andy Cheng <andytony56791@gmail.com>
2019-12-02 15:05:10 +0800

    cahnge to hash router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Main from "./containers/Main.js"
 import "./css/styles.css"
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <div className="App">
           <Main />
         </div>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
